c65f5844359985f0d98d63d5a06ce46a063468dc
Adam Paseka <pasekaadam@gmail.com>
2024-01-28 00:45:38 +0100

    make pause buttons prettier

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
 
+func _on_resume_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().root.get_child(1).unpause()
 
-func _on_play_pressed():
-	get_tree().root.get_child(1).unpause()
 
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_quit_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			get_tree().quit()
