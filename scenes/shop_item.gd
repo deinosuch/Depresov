7e396d6019fcad6b1eedd13debfd92f58b829dc6
Adam Paseka <pasekaadam@gmail.com>
2024-01-27 17:04:48 +0100

    make shop prettier

--- scenes/shop_item.gd
+++ scenes/shop_item.gd
@@ -1,23 +1,20 @@
 extends CenterContainer
 
-class_name ShopItem
-
 var item : Item
 @onready var game_state = get_node("/root/GameData")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("gui_input", _on_gui_input)
-	var rect = TextureRect.new()
-	rect.texture = item.texture
-	rect.set_expand_mode(1)
-	rect.custom_minimum_size = Vector2(100, 100)
-	add_child(rect)
+	pass
 
 func _on_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			game_state.money -= item.price
+			print(game_state.money)
 
-func _init(item_arg : Item):
+func initialize(item_arg : Item):
 	item = item_arg
+	var rect = $TextureRect
+	rect.texture = item.texture
+	add_child(rect)
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -5,10 +5,13 @@ extends Control
 func _ready():
 	var directory_path : String = "res://resources/items/"
 	
+	var i = 0
 	for filename in DirAccess.get_files_at(directory_path):
 		var item = load(directory_path + filename)
-		var shop_item = ShopItem.new(item)
-		$NinePatchRect/GridContainer.add_child(shop_item)
+		$Panel/GridContainer.get_child(i).initialize(item)
+		i+=1
+		if(i == 15):
+			break
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
