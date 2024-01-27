ea5c5adddfcd979c99d5d7b31d772de312ee45b4
Daniel Kamenov <sonnatherman04@gmail.com>
2024-01-27 18:33:24 +0100

    Rename stats

--- scripts/has_stats.gd
+++ scripts/has_stats.gd
@@ -2,20 +2,20 @@ extends Resource
 
 class_name HasStats
 
-@export var happiness = 100
+@export var mood = 100
 @export var energy = 100
 @export var health = 100
-@export var hunger = 100
+@export var focus = 100
 @export var anxiety = 100
 
-static var stat_names = ["Happiness", "Energy", "Health", "Hunger", "Anxiety"]
+static var stat_names = ["Mood", "Energy", "Health", "Focus", "Anxiety"]
 
 func get_stats():
-	return [happiness, energy, health, hunger, anxiety]
+	return [mood, energy, health, focus, anxiety]
 
 func add_stats(object : HasStats):
-	happiness = happiness + object.happiness
+	mood = mood + object.mood
 	energy = energy + object.energy
 	health = health + object.health
-	hunger = hunger + object.hunger
+	focus = focus + object.focus
 	anxiety = anxiety + object.anxiety
--- scripts/person.gd
+++ scripts/person.gd
@@ -6,24 +6,24 @@ class_name Person
 @export var name : String = ""
 @export var rights : bool = true
 @export var sprite : Texture2D
-var last_values = [happiness, energy, health, hunger, anxiety]
+var last_values = [mood, energy, health, focus, anxiety]
 @export var negative_penalty : float =  1.5# negative penalty multiplier
 
 func update_last_val():
-	last_values = [happiness, energy, health, hunger, anxiety]
+	last_values = [mood, energy, health, focus, anxiety]
 
 func print_out_stat():
-	print(happiness)
+	print(mood)
 	print(energy)
 	print(health)
-	print(hunger)
+	print(focus)
 	print(anxiety)
 
 func add_stats(object : HasStats):
-	happiness = max(0, min(100, happiness + object.happiness))
+	mood = max(0, min(100, mood + object.mood))
 	energy = max(0, min(100, energy + object.energy))
 	health = max(0, min(100, health + object.health))
-	hunger = max(0, min(100, hunger + object.hunger))
+	focus = max(0, min(100, focus + object.focus))
 	anxiety = max(0, min(100, anxiety + object.anxiety))
 	#var diff = get_difference()
 	#print(get_money_gain(diff))
@@ -48,7 +48,7 @@ func get_happy_gain(diff):
 	return sum
 
 func get_difference():
-	var diff = [happiness, energy, health, hunger, anxiety]
+	var diff = [mood, energy, health, focus, anxiety]
 	for i in range(len(diff)):
 		diff[i] -= last_values[i]
 	return diff
