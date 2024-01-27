91fc4b08219cb0408ae3eda4946e99dd9033b28e
Daniel Kamenov <sonnatherman04@gmail.com>
2024-01-27 17:05:44 +0100

    add item assets

--- People/queue.gd
+++ People/queue.gd
@@ -5,7 +5,7 @@ class_name PersonQueue
 @export var queue : Array[Person]
 
 func next():
-	var person = queue.pop_back()
+	var person = queue.pop_front()
 	$Person.texture = person.sprite
 	return person
 
