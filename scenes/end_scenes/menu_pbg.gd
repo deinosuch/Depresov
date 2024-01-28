4b01102ec9d462bff3ef5fac2732061b630bac1f
Adam Paseka <pasekaadam@gmail.com>
2024-01-28 14:26:49 +0100

    merge credits

--- scenes/end_scenes/menu_pbg.gd
+++ scenes/end_scenes/menu_pbg.gd
@@ -3,4 +3,4 @@ extends Control
 func _on_nine_patch_rect_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			GameData.quit()
+			GameData.goto("main_menu")
