206b4f02ee18ed66a238f93f644ab277dceea4bb
Daniel Kamenov <sonnatherman04@gmail.com>
2024-01-28 12:37:22 +0100

    Item SoundFX

--- scripts/inventory/item.gd
+++ scripts/inventory/item.gd
@@ -6,6 +6,7 @@ class_name Item
 @export var name: String = ""
 @export var texture: Texture2D
 @export var price = 10
+@export var sound: AudioStream
 
 func use(person: Person):
 	person.add_stats(self)
